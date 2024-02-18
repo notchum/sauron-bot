1b2c66423ac93461d384fe38001aabbaf30762f2
notchum <jmrc98@gmail.com>
2024-02-17 19:41:35 -0700

    fix search commands. add pagination

--- cogs/commands.py
+++ cogs/commands.py
@@ -5,6 +5,7 @@ import disnake
 from disnake.ext import commands
 
 from bot import SauronBot
+from views import Paginator
 from helpers import ImageProcessor, VideoProcessor
 from helpers.utilities import validate_attachment, get_content_type, ContentType
 
@@ -12,23 +13,96 @@ class Commands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: SauronBot = bot
 
-    async def download_image(self, url: str) -> str | None:
+    async def download_media(self, url: str) -> str | None:
         try:
-            image_filename = os.path.basename(url).split("?")[0]
-            image_path = os.path.join(self.cache_dir, image_filename)
+            media_filename = os.path.basename(url).split("?")[0]
+            media_path = os.path.join(self.bot.cache_dir, media_filename)
 
-            if not os.path.exists(image_path):
-                async with self.session.get(url) as response:
+            if not os.path.exists(media_path):
+                async with self.bot.session.get(url) as response:
                     if response.status != 200:
-                        self.logger.error(f"Downloading image {url} returned status code `{response.status}`")
+                        self.bot.logger.error(f"Downloading media {url} returned status code `{response.status}`")
                         return None
-                    async with aiofiles.open(image_path, mode="wb") as f:
+                    async with aiofiles.open(media_path, mode="wb") as f:
                         await f.write(await response.read())
-                    self.logger.info(f"Downloaded image {image_path}")
-            return image_path
+            return media_path
         except Exception as err:
-            self.logger.error(f"Downloading image returned invalid data! {err}")
+            self.bot.logger.error(f"Downloading media returned invalid data! {err}")
             return None
+
+    async def get_attachment_hash(self, attachment: disnake.Attachment) -> int:
+        file_path = os.path.join(self.bot.cache_dir, attachment.filename)
+        await attachment.save(fp=file_path, use_cached=True)
+
+        content_type = get_content_type(attachment)
+        if content_type is None:
+            raise ValueError(f"Attachment {attachment.filename} has invalid content type {attachment.content_type}")
+
+        # Process the image or video
+        if content_type == ContentType.IMAGE:
+            imageproc = ImageProcessor(file_path)
+            hash = imageproc.hash
+        elif content_type == ContentType.VIDEO:
+            try:
+                videoproc = VideoProcessor(file_path)
+            except:
+                raise ValueError(f"Failed to process video {attachment.filename}")
+            hash = videoproc.hash
+        else:
+            raise ValueError(f"Attachment {attachment.filename} has invalid content type {attachment.content_type}")
+        
+        return hash
+
+    async def find_similar_images(self, hash: int, max_hamming_distance: int, guild_id: int) -> list[dict[str, str]]:
+        query = """
+            SELECT *
+            FROM media_metadata
+            WHERE hash <@ ($1, $2)
+            AND guild_id = $3;
+        """
+        matches = await self.bot.execute_query(query, hash, max_hamming_distance, guild_id)
+        return matches
+
+    async def send_search_results(self, inter: disnake.ApplicationCommandInteraction, matches: list[dict[str, str]], title: str, image_url: str = None):
+        if not matches:
+            await inter.edit_original_response("No similar images or videos found.")
+            return
+
+        matches.sort(key=lambda match: match['timestamp'])
+        
+        embeds = []
+        message_urls = []
+        for i, match in enumerate(matches, 1):
+            user_mention = f"<@{match['author_id']}>"
+            #TODO add 'via bot' if the message was sent by the bot
+            time_sent = f"<t:{int(match['timestamp'].timestamp())}:F>"
+            jump_url = f"https://discord.com/channels/{match['guild_id']}/{match['channel_id']}/{match['message_id']}"
+            
+            message_urls.append(f"{i}. {jump_url} ({time_sent})\n  - Author: {user_mention}")
+            if i % 10 == 0:
+                embed = disnake.Embed(
+                    title=title,
+                    description="\n".join(message_urls),
+                    color=disnake.Color.dark_orange(),
+                )
+                if image_url:
+                    embed.set_thumbnail(url=image_url)
+                embeds.append(embed)
+                message_urls = []
+        if message_urls:
+            embed = disnake.Embed(
+                title=title,
+                description="\n".join(message_urls),
+                color=disnake.Color.dark_orange(),
+            )
+            if image_url:
+                embed.set_thumbnail(url=image_url)
+            embeds.append(embed)
+        
+        paginator_view = Paginator(embeds, inter.author)
+        message = await inter.edit_original_response(content=f"Found `{len(matches)}` results.", embed=embeds[0], view=paginator_view)
+        paginator_view.message = message
+        return
 
     @commands.slash_command()
     async def search_text(
@@ -76,41 +150,30 @@ class Commands(commands.Cog):
                 matches.append(match)
         self.bot.logger.info(f"Found {len(matches)} similar images.")
 
-        if not matches:
-            await inter.edit_original_response("No similar images found.")
-            return
-        
-        message_urls = []
-        for i, match in enumerate(matches, 1):
-            user_mention = f"<@{match['author_id']}>"
-            time_sent = f"<t:{int(match['timestamp'].timestamp())}:F>"
-            jump_url = f"https://discord.com/channels/{match['guild_id']}/{match['channel_id']}/{match['message_id']}"
-            message_urls.append(f"{i}. By {user_mention} on {time_sent} in {jump_url}")
-        message_urls = "\n".join(message_urls)
-
-        embed = disnake.Embed(
-            title=f"Search Results",
-            description=f"Found `{len(matches)}` similar images.\n{message_urls}",
-            color=disnake.Color.dark_orange(),
-        )
-        await inter.edit_original_response(embed=embed)
-        return
+        await self.send_search_results(inter, matches, "Search Results")
     
     @commands.slash_command()
-    async def search_image(
+    async def search_attachment(
         self,
         inter: disnake.ApplicationCommandInteraction,
-        image: disnake.Attachment,
-        max_hamming_distance: int = 10
+        attachment: disnake.Attachment,
+        max_hamming_distance: int = 0
     ):
-        """ Search for an image based on an image. """
+        """ Search for an image or video based on any message attachment.
+        
+            Parameters
+            ----------
+            attachment: `disnake.Attachment`
+                The attachment to search for.
+            max_hamming_distance: `int`
+                The maximum Hamming distance to use when searching.
+        """
         await inter.response.defer()
 
-        image_path = os.path.join(self.bot.cache_dir, image.filename)
-        await image.save(fp=image_path, use_cached=True)
-        
-        imageproc = ImageProcessor(image_path)
-        hash = imageproc.hash
+        file_path = os.path.join(self.bot.cache_dir, attachment.filename)
+        await attachment.save(fp=file_path, use_cached=True)
+
+        hash = await self.get_attachment_hash(attachment)
 
         query = """
             SELECT *
@@ -146,99 +209,71 @@ class Commands(commands.Cog):
         self,
         inter: disnake.ApplicationCommandInteraction,
         image_url: str,
-        max_hamming_distance: int = 10
+        max_hamming_distance: int = 0
     ):
-        """ Search for an image based on any image URL. """
+        """ Search for an image or video with a URL to an image or video.
+
+            Parameters
+            ----------
+            image_url: `str`
+                The URL to the image or video to search for.
+            max_hamming_distance: `int`
+                The maximum Hamming distance to use when searching.
+        """
         await inter.response.defer()
         
-        image_path = await self.download_image(image_url)
+        image_path = await self.download_media(image_url)
+        self.bot.logger.info(f"Downloaded image {image_path}")
 
-        imageproc = ImageProcessor(image_path)
-        hash = imageproc.hash
+        image_host_channel = await self.bot.fetch_channel(1164613880538992760)
+        image_host_msg = await image_host_channel.send(file=disnake.File(image_path))
+        attachment = image_host_msg.attachments[0]
+        self.bot.logger.info(f"Uploaded image {image_host_msg.attachments[0].url}")
 
-        query = """
-            SELECT *
-            FROM media_metadata
-            WHERE hash <@ ($1, $2)
-            AND guild_id = $3;
-        """
-        matches = await self.bot.execute_query(query, hash, max_hamming_distance, inter.guild.id)
+        hash = await self.get_attachment_hash(attachment)
+        self.bot.logger.info(f"Hash: {hash}")
+
+        matches = await self.find_similar_images(hash, max_hamming_distance, inter.guild.id)
         self.bot.logger.info(f"Found {len(matches)} similar images.")
 
-        if not matches:
-            await inter.edit_original_response("No similar images found.")
-            return
-
-        message_urls = []
-        for i, match in enumerate(matches, 1):
-            user_mention = f"<@{match['author_id']}>"
-            time_sent = f"<t:{int(match['timestamp'].timestamp())}:F>"
-            jump_url = f"https://discord.com/channels/{match['guild_id']}/{match['channel_id']}/{match['message_id']}"
-            message_urls.append(f"{i}. By {user_mention} on {time_sent} in {jump_url}")
-        message_urls = "\n".join(message_urls)
-        
-        embed = disnake.Embed(
-            title=f"Search Results",
-            description=f"Found `{len(matches)}` similar images.\n{message_urls}",
-            color=disnake.Color.dark_orange(),
-        ).set_thumbnail(url=image_url)
-        await inter.edit_original_response(embed=embed)
+        await self.send_search_results(inter, matches, "Search Results", attachment.url)
         return
 
     @commands.slash_command()
     async def search_message(
         self,
         inter: disnake.ApplicationCommandInteraction,
-        message: disnake.Message
+        message: disnake.Message,
+        max_hamming_distance: int = 0
     ):
-        """ Search for an image based on a message. """
+        """ Search for an image or video based on a message attachment.
+        
+            Parameters
+            ----------
+            message: `disnake.Message`
+                The message with the attachment to search for.
+            max_hamming_distance: `int`
+                The maximum Hamming distance to use when searching.
+        """
         await inter.response.defer()
 
         if not message.attachments:
-            await inter.edit_original_response("No attachments found.")
+            await inter.edit_original_response(f"No attachments found in {message.jump_url}.")
             return
-
-        for attachment in message.attachments:
-            if attachment.content_type is None:
-                if not attachment.filename.lower().endswith((".png", ".jpg", ".jpeg")):
-                    continue
-            elif not attachment.content_type.startswith("image"):
-                continue
+        attachment = message.attachments[0] #TODO: Add support for multiple attachments
             
-            image_path = os.path.join(self.bot.cache_dir, attachment.filename)
-            await attachment.save(fp=image_path, use_cached=True)
-            
-            imageproc = ImageProcessor(image_path)
-            hash = imageproc.hash
+        image_path = os.path.join(self.bot.cache_dir, attachment.filename)
+        await attachment.save(fp=image_path, use_cached=True)
+        self.bot.logger.info(f"Saved attachment {attachment.filename} from message {message.id} to {image_path}")
+        
+        hash = await self.get_attachment_hash(attachment)
+        self.bot.logger.info(f"Hash: {hash}")
 
-            query = """
-                SELECT *
-                FROM media_metadata
-                WHERE hash <@ ($1, $2)
-                AND guild_id = $3;
-            """
-            matches = await self.bot.execute_query(query, hash, 0, inter.guild.id)
-            self.bot.logger.info(f"Found {len(matches)} similar images.")
+        matches = await self.find_similar_images(hash, max_hamming_distance, inter.guild.id)
+        self.bot.logger.info(f"Found {len(matches)} similar images.")
 
-            if not matches:
-                await inter.edit_original_response("No similar images found.")
-                return
-
-            message_urls = []
-            for i, match in enumerate(matches, 1):
-                user_mention = f"<@{match['author_id']}>"
-                time_sent = f"<t:{int(match['timestamp'].timestamp())}:F>"
-                jump_url = f"https://discord.com/channels/{match['guild_id']}/{match['channel_id']}/{match['message_id']}"
-                message_urls.append(f"{i}. By {user_mention} on {time_sent} in {jump_url}")
-            message_urls = "\n".join(message_urls)
-            
-            embed = disnake.Embed(
-                title=f"Search Results",
-                description=f"Found `{len(matches)}` similar images.\n{message_urls}",
-                color=disnake.Color.dark_orange(),
-            ).set_thumbnail(url=attachment.url)
-            await inter.edit_original_response(embed=embed)
-            return
+        await self.send_search_results(inter, matches, "Search Results", attachment.url)
+        return
 
     @commands.slash_command(default_member_permissions=disnake.Permissions(administrator=True))
     async def view_info(
@@ -338,12 +373,12 @@ class Commands(commands.Cog):
                     # Update the content type and filename in the database
                     query = """
                         UPDATE media_metadata
-                        SET content_type = $1, filename = $2
-                        WHERE message_id = $3
-                        AND channel_id = $4
-                        AND guild_id = $5;
+                        SET content_type = $1, filename = $2, timestamp = $3
+                        WHERE message_id = $4
+                        AND channel_id = $5
+                        AND guild_id = $6;
                     """
-                    await self.bot.execute_query(query, attachment.content_type, attachment.filename, message.id, channel.id, message.guild.id)
+                    await self.bot.execute_query(query, attachment.content_type, attachment.filename, message.created_at, message.id, channel.id, message.guild.id)
                     self.bot.logger.info(f"Message {message.id}: Updated content type and filename in database.")
                     continue
 
